7231a060b6134e832b024f5ecc03cf6e35f33ded
muzammilaalu <mujmmil251@email.com>
2026-03-13 11:17:08 +0530

    api update

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -52,7 +52,7 @@
 
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = "https://clearbook-backend-f9e8.onrender.com"
 
 const api = axios.create({
   baseURL: API_BASE_URL,
